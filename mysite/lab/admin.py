676a9dd012d99c032e8e38d8e8cf026b6f03b4e7
XanDuri <foxtoja1098765432@gmail.com>
2024-10-17 09:56:10 +0200

    17.10.2024

--- mysite/lab/admin.py
+++ mysite/lab/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 
-from .models import Team, Shop
+from .models import Team, Shop, Osoba, Stanowisko
 
 admin.site.register(Team)
 admin.site.register(Shop)
+
+
+class OsobaAdmin(admin.ModelAdmin):
+    readonly_fields = ["data_dodania"]
+    list_display = ["data_dodania", "nazwisko", "imie", "stanowisko", "plec"]
+    list_filter = ["stanowisko", "data_dodania"]
+
+    @admin.display(description="Dodana")
+    def data_dodania(self, instance):
+        return instance
+
+    @admin.display(description="Stanowisko")
+    def stanowisko(self, instance):
+        return f"{instance.stanowisko}+{instance.id}"
+
+# Dodanie 2 nowych modeli do panelu administratora
+admin.site.register(Osoba, OsobaAdmin)
+
+class StanowiskoAdmin(admin.ModelAdmin):
+    list_display = ["nazwa", "opis"]
+    list_filter = ["nazwa"]
+
+admin.site.register(Stanowisko, StanowiskoAdmin)
 # Register your models here.
--- mysite/lab/models.py
+++ mysite/lab/models.py
@@ -1,7 +1,11 @@
+from datetime import timezone, datetime, date
+
 from django.db import models
+
 
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+GENDER = models.IntegerChoices('Płeć', 'Kobieta Mężczyzna Inne')
 
 SHIRT_SIZES = (
         ('S', 'Small'),
@@ -32,3 +36,27 @@ class Shop(models.Model):
     name = models.CharField(max_length=20)
     pub_date = models.DateField("Date Published")
     workers = models.IntegerField(default=0)
+
+#Zadamie 1
+#Model Stanowisko
+class Stanowisko(models.Model):
+    nazwa = models.CharField(null=False, blank=False, max_length=20)
+    opis = models.CharField(null=True, blank=True, max_length=100)
+
+    def __str__(self):
+        return f"{self.nazwa}"
+
+#Model Osoba
+class Osoba(models.Model):
+    imie = models.CharField(null=False, blank=False, max_length=20)
+    nazwisko = models.CharField(null=False, blank=False, max_length=20)
+    plec = models.IntegerField(choices=GENDER.choices)
+    stanowisko = models.ForeignKey(Stanowisko, null=True, blank=True, on_delete=models.SET_NULL)
+    data_dodania = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.imie + " " + self.nazwisko}"
+
+    class Meta:
+        ordering = ["nazwisko"]
+
